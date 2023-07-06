5717f0ecb346c9b01bcc5ddf01104eac5b44af02
yatin2211 <134246475+yatin2211@users.noreply.github.com>
2023-07-06 11:54:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -20,16 +20,6 @@ const streamifier = require("streamifier");
 const path = require("path");
 const blogData = require("./blog-service");
 const stripJs = require('strip-js');
-const {
-  initialize,
-  getAllPosts,
-  getCategories,
-  addPost,
-  getPostById,
-  getPostsByCategory,
-  getPostsByMinDate,
-} = require("./blog-service.js");
-
 
 const app = express();
 
